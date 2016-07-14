1648a0ccd58fde8a67f8979f60b3bec82f1a84e8
charan <charan100reddy@gmail.com>
2016-07-14 07:43:01 +0530

    master changed

--- myproject/src/myproject/myclass.java
+++ myproject/src/myproject/myclass.java
@@ -4,7 +4,8 @@ public class myclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("my first class");
+		System.out.println("my first class ");
+		System.out.println("change for merge ");
 	}
 
 }
