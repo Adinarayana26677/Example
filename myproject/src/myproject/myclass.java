eb992a1224720b518335d6ddaa0d3d7078840f8f
charan <charan100reddy@gmail.com>
2016-07-14 07:40:19 +0530

    branch commit

--- myproject/src/myproject/myclass.java
+++ myproject/src/myproject/myclass.java
@@ -5,6 +5,7 @@ public class myclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("my first class");
+		System.out.println("my first class in branch12");
 	}
 
 }
